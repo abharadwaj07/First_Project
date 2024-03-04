f8a093facf98d762c8738943a6663b21ad4e8a78
Aashish <aashishbharadwaj@outlook.com>
2024-03-04 12:23:40 -0500

    app.py and web application

--- src/utils.py
+++ src/utils.py
@@ -44,3 +44,18 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
     except Exception as e:
         logging.info('Exception occured during model training')
         raise CustomException(e,sys)
+    
+
+def load_object(file_path):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        logging.info('Exception occured in load_object function utils')
+        raise CustomException(e, sys)
+    
